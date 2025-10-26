6f09289f8bfaff5f16e55bf8fb87842a52412d20
mariawagas <mar1awagas7@gmail.com>
2025-10-26 14:56:40 +0800

    Update Transaction.java

--- Eclipse/ACCTG21/src/ACCTG/Transaction.java
+++ Eclipse/ACCTG21/src/ACCTG/Transaction.java
@@ -1,10 +1,7 @@
 package ACCTG;
 
 public class Transaction {
-    private String date;
-    private String description;
-    private String debitAccount;
-    private String creditAccount;
+    private String date, description, debitAccount, creditAccount;
     private double amount;
 
     public Transaction(String date, String description, String debitAccount, String creditAccount, double amount) {
@@ -15,12 +12,13 @@ public class Transaction {
         this.amount = amount;
     }
 
-    // Getters
     public String getDate() { return date; }
     public String getDescription() { return description; }
     public String getDebitAccount() { return debitAccount; }
     public String getCreditAccount() { return creditAccount; }
     public double getAmount() { return amount; }
+}
+
 
     // Setters (if needed)
     public void setDate(String date) { this.date = date; }
@@ -29,3 +27,4 @@ public class Transaction {
     public void setCreditAccount(String creditAccount) { this.creditAccount = creditAccount; }
     public void setAmount(double amount) { this.amount = amount; }
 }
+
